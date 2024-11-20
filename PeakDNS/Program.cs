188416f86df55b712c2f0323ebb77a764540f5e0
Joar Heimonen <contact@joar.me>
2024-11-20 21:46:13 +0100

    fix: added ;

--- PeakDNS/Program.cs
+++ PeakDNS/Program.cs
@@ -46,7 +46,7 @@ namespace PeakDNS
                 
                 //check if the answer is in the cache
                 if(cache.hasAnswer(_packet)) {
-                    logger.Debug("Anwser is cached")
+                    logger.Debug("Anwser is cached");
                     //get the answer from the cache
                     Answer[] answers = cache.getAnswers(_packet);
                     //set the answers
@@ -71,7 +71,7 @@ namespace PeakDNS
 
                     return;
                 }
-                logger.Debug("Anwser is not cached")
+                logger.Debug("Anwser is not cached");
 
 
                 //loop trough all zones and check if one of them can answer the question
