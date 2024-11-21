180699732702d3c98d34278f19a51a2cda349259
Joar Heimonen <contact@joar.me>
2024-11-21 02:09:02 +0100

    feat: generate short hash method

--- PeakDNS/Kubernetes/Reader.cs
+++ PeakDNS/Kubernetes/Reader.cs
@@ -56,3 +56,19 @@ namespace PeakDNS.Kubernetes
         }
     }
 }
+
+private static string GenerateShortHash(string input)
+{
+    using (var md5 = MD5.Create())
+    {
+        byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+        byte[] hashBytes = md5.ComputeHash(inputBytes);
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 4; i++)
+        {
+            sb.Append(hashBytes[i].ToString("x2"));
+        }
+        return sb.ToString();
+    }
+}
